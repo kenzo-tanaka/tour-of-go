eaedcecb60bb8677589fa5609d15886c8eba8458
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-16 20:28:46 +0900

    Add comment

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	i := 42
+	i := 5
 	pointer := &i
-	fmt.Println(pointer)
+	fmt.Println(*pointer)
+
+	// ポインタを操作することで
+	// ポインタが指す変数の値を直接変更することができる
+	*pointer = 21
+	fmt.Println(i)
 }
